e72dbb065af995ef83ea34b4c83720822878dcb2
a <a@example.com>
2022-08-25 06:12:21 +0900

    emit event in setCollateralBalance

--- contracts/test/DebugPerpdexExchange.sol
+++ contracts/test/DebugPerpdexExchange.sol
@@ -2,9 +2,12 @@
 pragma solidity >=0.7.6;
 pragma abicoder v2;
 
+import { PerpMath } from "../lib/PerpMath.sol";
 import { PerpdexExchange } from "../PerpdexExchange.sol";
 
 contract DebugPerpdexExchange is PerpdexExchange {
+    event CollateralBalanceSet(address indexed trader, int256 beforeBalance, int256 afterBalance);
+
     uint256 private constant _RINKEBY_CHAIN_ID = 4;
     uint256 private constant _MUMBAI_CHAIN_ID = 80001;
     uint256 private constant _SHIBUYA_CHAIN_ID = 81;
@@ -13,6 +16,8 @@ contract DebugPerpdexExchange is PerpdexExchange {
     uint256 private constant _ARBITRUM_RINKEBY_CHAIN_ID = 421611;
     uint256 private constant _OPTIMISM_KOVAN_CHAIN_ID = 69;
     uint256 private constant _HARDHAT_CHAIN_ID = 31337;
+
+    int256 private constant MAX_COLLATERAL_BALANCE = 10000e18;
 
     constructor(address settlementTokenArg) PerpdexExchange(msg.sender, settlementTokenArg, new address[](0)) {
         uint256 chainId;
@@ -32,6 +37,8 @@ contract DebugPerpdexExchange is PerpdexExchange {
     }
 
     function setCollateralBalance(address trader, int256 balance) external {
+        balance = PerpMath.min(balance, MAX_COLLATERAL_BALANCE);
+        emit CollateralBalanceSet(trader, accountInfos[trader].vaultInfo.collateralBalance, balance);
         accountInfos[trader].vaultInfo.collateralBalance = balance;
     }
 }
